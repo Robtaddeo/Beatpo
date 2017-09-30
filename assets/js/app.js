2e7854565c0ff1ebc1240b05dccb0c5bf518e73b
Robert Taddeo <robtaddeo@gmail.com>
2017-09-30 16:37:05 -0400

    Test commit with atom

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@
 (function() {
 
   console.log('Hello Beatpo!')
+  var currentUser = null
 
   var turbo = Turbo({ site_id: '59ce9f1f448851001216bdb7' })
   turbo.currentUser(function(err, data) {
@@ -11,12 +12,53 @@
       return
     }
 
+    currentUser = data.user
     $('#header-username').html(data.user.name)
+
+    var tracksList = ''
+    currentUser.tracks.forEach(function(track, i){
+      tracksList += '<tr><td style="width: 130px;"><a target"_blank" href="#"><img src="/dist/images/playbutton.png" alt="..."></a></td>'
+      tracksList += '<td><h5>'+track.name+'</h5><p>Superior Sports Watch</p></td>'
+      tracksList += '<td><label>Plays: </label><p></p></td>'
+      tracksList += '<td><a><h4 class="price">$40</h4></a></td>'
+    })
+
+    $('#tracks-table').html(tracksList)
   })
 
   $('#btn-upload').click(function(event) {
     event.preventDefault()
     console.log('Upload Track')
+
+    turbo.uploadFile(function(err, data){
+      if (err) {
+        alert('Error: ' + err.message)
+        return
+      }
+
+      console.log('Upload Complete: ' + JSON.stringify(data))
+      var file = data.result
+
+      if (currentUser == null){
+        return
+      }
+
+      // Update Current User
+      var tracks = currentUser.tracks || []
+      tracks.push(file)
+
+      turbo.update('user', currentUser, {tracks: tracks}, function(err, data){
+        if (err){
+          alert('Error: ' + err.message)
+          return
+        }
+
+          console.log('USER UPDATED: ' + JSON.stringify(data))
+      })
+
+    })
+
+
   })
 
 
